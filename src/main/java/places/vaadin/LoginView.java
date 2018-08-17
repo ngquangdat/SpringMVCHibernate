1fdfa32267d40726c404807eae093c6a3a16a513
ngquangdat <ngquangdat13@gmail.com>
2018-08-17 07:01:37 +0700

    add 17082018

--- src/main/java/places/vaadin/LoginView.java
+++ src/main/java/places/vaadin/LoginView.java
@@ -1,16 +1,20 @@
 package places.vaadin;
 
-import javax.servlet.annotation.WebInitParam;
+
 import javax.servlet.annotation.WebServlet;
+
+import org.springframework.context.annotation.ComponentScan;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinServletConfiguration;
 
 
@@ -19,7 +23,9 @@ import com.vaadin.flow.server.VaadinServletConfiguration;
 
 @SuppressWarnings("serial")
 @Theme("placestheme")
-public class LoginView extends VerticalLayout{
+@Route("/login")
+
+public class LoginView extends UI {
 	
 	public LoginView() {
 		final TextField username = new TextField("Tên đăng nhập");
@@ -33,20 +39,11 @@ public class LoginView extends VerticalLayout{
 	}
 	
 	
-	public class LoginUI extends UI{
-		protected void init(VaadinRequest vaadinRequest) {
-			LoginView loginView = new LoginView();
-			add(loginView);
-		}
-		
-	}
 	
-	@WebServlet(urlPatterns = "/login/*", name = "LoginUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = LoginUI.class, productionMode = false)
+	@WebServlet(urlPatterns = "/login/*", name = "loginServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = LoginView.class, productionMode = false)
     public static class LoginUIServlet extends VaadinServlet {
     }
-	
-	
 }
 
 
